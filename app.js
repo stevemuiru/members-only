1ef85e9a00b5c91cf2aa2d2e8a0687b4f5770bf7
stephen muiru <stevembugua26@gmail.com>
2026-06-22 15:02:21 +0300

    intial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const path = require("node:path");
+const express = require('express');
+const app = express();
+const { Pool } = require("pg");
+
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render('index'));
+
+app.listen(3000, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log("app listening on port 3000!");
+});
